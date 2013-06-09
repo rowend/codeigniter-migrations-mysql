2b476f0d15955d8d9d37f0a2e8b65209cdce957d
rowend <rowend07@gmail.com>
2013-06-09 18:22:03 -0500

    innodb table

--- mymigration.php
+++ mymigration.php
@@ -2,8 +2,20 @@
 
     class MyMigration extends CI_Migration {
 
-        function create_table() {
+        function innodb($config) {
+            if(isset($config['innodb']) && $config['innodb'] === TRUE){
+                $table = $config['table'];
+                $sql = "ALTER TABLE  `$table` ENGINE = INNODB";
+                $this->db->query($sql);
+            }
+        }
 
+        function create_table() {
+            $fields = $config['fields'];
+            $table = $config['table'];
+            $this->dbforge->add_field($fields);
+            $this->dbforge->create_table($table, TRUE);
+            $this->innodb($config);
         }
 
     }
